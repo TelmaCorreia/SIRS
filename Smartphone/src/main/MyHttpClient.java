3fc3bb2e6aad90079c2125bc892ee8b8ea0f76a0
TelmaCorreia <t.correia222@gmail.com>
2017-10-31 16:56:55 +0000

    httpclient v1

--- Smartphone/src/main/MyHttpClient.java
+++ Smartphone/src/main/MyHttpClient.java
@@ -4,9 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 public class MyHttpClient {
@@ -20,9 +25,11 @@ public class MyHttpClient {
 			
 			HttpClient client = HttpClientBuilder.create().build();
 			HttpGet request = new HttpGet(url);
-
+			String msg = "";
 			// add request header
 			request.addHeader("User-Agent", USER_AGENT);
+			request.addHeader("content-lenght",String.valueOf(msg.length()));
+			
 			HttpResponse response = client.execute(request);
 
 			System.out.println("Response Code : "
@@ -44,5 +51,31 @@ public class MyHttpClient {
 
 		
 	}
+	
+	public void POSTrequest(){
+		 HttpClient client = new DefaultHttpClient();
+		 
+	     HttpPost post = new HttpPost("localhost");
+        try {
+        	String msg = "Ola";
+			post.addHeader("User-Agent", USER_AGENT);
+            post.addHeader("content-lenght",String.valueOf(msg.length()));
+            post.setEntity(new StringEntity(msg));
+
+            HttpResponse response = client.execute(post);
+            BufferedReader rd = new BufferedReader(new InputStreamReader(
+                    response.getEntity().getContent()));
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+		
+	
 	
 }
