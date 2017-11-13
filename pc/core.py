1d44cdeb7c6a860615eca03f7777f553b000520a
filipepgm <filipepgm@hotmail.com>
2017-11-13 15:19:02 +0000

    pc: extra verifications and stub rsa

--- pc/core.py
+++ pc/core.py
@@ -1,5 +1,5 @@
 import file_encription
-import symmetric
+import my_crypto
 
 # Configuration
 folder = "folder"
@@ -7,20 +7,20 @@ seconds_until_expiration = 10
 key_size = 16
 
 # Session storage
-stored_key = "0123456701234567"
-stored_iv = "0123456701234567"
-session_key = None
-session_iv = None
+stored_key = "0123456701234567" # FIXME ALL "" by default
+session_key = "0123456701234567"
+session_iv = "0123456701234567"
 
 def assert_validkey(key):
     if len(key) != key_size:
         raise Exception("Wrong key size", len(key))
+    if stored_key != "":
+        raise Exception("Folder key already defined")
 
 def on_receiving_key(key):
     global stored_key
     stored_key = key
     file_encription.decrypt_files(key, folder)
-    # TODO start timer
 
 def on_close():
     global stored_key
@@ -28,16 +28,35 @@ def on_close():
     stored_key = ""
     # TODO stop timer
 
+def start_session(message):
+    if len(message) != key_size*2:
+        raise Exception("Wrong session key size, receieved bytes:", len(message))
+    global session_iv
+    global session_key
+    session_key = message[:key_size]
+    session_iv = message[key_size:]
+    # TODO start timer
+    return "OKOK"
+
+def refresh_timer():
+    pass
+
 def process_raw(text):
     """Throws exceptions on incorrect messages! Must be handled above"""
     if session_key:
-        message = symmetric.decompose_message(text, session_key, session_iv)
+        message = my_crypto.decompose_message(text, session_key, session_iv)
         response = process_message(message)
-        return symmetric.compose_message(response, session_key, session_iv)
+        reply =  my_crypto.compose_message(response, session_key, session_iv)
+        if response == "STOP":
+            global session_iv
+            global session_key
+            session_iv = ""
+            session_key = ""
+        return reply
     else:
-        #TODO
-        return "TODO, create signed response"
-
+        message = my_crypto.decompose_start(text)
+        response = start_session(message)
+        return my_crypto.compose_start(response, session_key, session_iv)
 
 def process_message(message):
     if message == "":
@@ -55,7 +74,7 @@ def process_message(message):
         return "STOP"
 
     elif message == "PING":
+        refresh_timer()
         return "PING"
-
     else:
         raise Exception("Received unkonwn string", message)
--- pc/my_crypto.py
+++ pc/my_crypto.py
@@ -1,11 +1,14 @@
-from Crypto.Cipher import AES
-from  Crypto.Random import random
 import struct
 import time
+from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA256
-import base64
+from Crypto.Random import random
+
+rsa_key_file = ""
 
 def hash_text(text):
+    """SHA256 hash"""
     hash = SHA256.new()
     hash.update(text)
     return hash.digest()
@@ -15,33 +18,18 @@ tolerance_seconds = 3
 timestamp_format = "I"
 
 def is_fresh(timestamp):
+    """Checks if timestamp is within the tolerance"""
     now = int(time.time())
-
     return abs(now-timestamp) <= tolerance_seconds
 
 def time_message(message):
+    """Returns timestamp+message"""
     timestamp = int(time.time())
     nonced_message = struct.pack('<'+timestamp_format, timestamp) + message
     return nonced_message
 
-def hash_message(message):
-    return hash_text(message) + message
-
-def check_hash_message(text):
-    hash_size = 32
-    if len(text) < hash_size:
-        raise Exception("Hash", "Message too small")
-
-    received_hash = text[:hash_size]
-
-    actual_hash = hash_text(text[hash_size:])
-
-    if received_hash != actual_hash:
-        raise Exception("Hash", "Mismatching Hash!")
-
-    return text[hash_size:]
-
 def check_time_message(text):
+    """Assumes timestamp+message. Checks if the timestamp is fresh"""
     timestamp_size = struct.calcsize(timestamp_format)
     timestamp_text = text[:timestamp_size]
     received_timestamp = struct.unpack('<'+timestamp_format, timestamp_text)[0]
@@ -51,14 +39,36 @@ def check_time_message(text):
     message = text[timestamp_size:]
     return message
 
+######################################### INTEGRITY #########################################
+def hash_message(message):
+    """Returns hash+message"""
+    return hash_text(message) + message
+
+def sign_message(message):
+    """Returns signed_hash+message"""
+    rsa_key = RSA.importKey(open(rsa_key_file, "rb").read())
+    message_hash = hash_text(message)
+    signature = rsa_key.decrypt(message_hash) # TODO switch to recommended signature. proper padding
+    return signature + message
+
+def check_hash_message(text):
+    """Assumes text is "hash+message". checls if message corresponds to the given hash."""
+    hash_size = 32
+    if len(text) < hash_size:
+        raise Exception("Hash", "Message too small")
+
+    received_hash = text[:hash_size]
+    actual_hash = hash_text(text[hash_size:])
+
+    if received_hash != actual_hash:
+        raise Exception("Hash", "Mismatching Hash!")
+
+    return text[hash_size:]
 ######################################### Encryption #########################################
 def encrypt_string(message="", key="0123456701234567", iv="0123456701234567"):
     """Returns a string with encrypted message(AES CBC). adds padding as needed"""
-    # secure random iv
-    #iv = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
-    message_size = len(message)
     encryptor = AES.new(key, AES.MODE_CBC, iv)
-    # padding
+    
     padding_size = 16 - len(message) % 16
     message += chr(padding_size) * padding_size
 
@@ -82,15 +92,27 @@ def compose_message(message, key="0123456701234567", iv="0123456701234567"):
     hashed = hash_message(timed)
     encrypted = encrypt_string(hashed, key, iv)
     return encrypted
-    #return base64.b64encode(encrypted)
 
 def decompose_message(text, key="0123456701234567", iv="0123456701234567"):
-    #raw = base64.b64decode(text)
     decrypted = decrypt_string(text, key, iv)
     correct_message = check_hash_message(decrypted)
     fresh_message = check_time_message(correct_message)
     return fresh_message
 
+def decompose_start(message):
+    #rsa_key = RSA.importKey(open(rsa_key_file, "rb").read())
+    #decrypted =  rsa_key.decrypt(message) # TODO swith to recommended. proper padding
+
+    decrypted = message # TODO RSA
+    correct_message = check_hash_message(decrypted)
+    fresh_message = check_time_message(correct_message)
+    return fresh_message
+
+def compose_start(message, key, iv):
+    timed = time_message(message)
+    hashed = sign_message(timed)
+    encrypted = encrypt_string(hashed, key, iv)
+    return encrypted
 
 #print encrypt_string("A")
 #print decrypt_string(encrypt_string("A"))
