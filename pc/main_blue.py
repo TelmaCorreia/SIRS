1f0cab6051bcb42bf78cc05fd6279ebbea29cc6c
filipepgm <filipepgm@hotmail.com>
2017-11-28 14:41:08 +0000

    pc: functionl (?) bluetooth server

--- pc/main_blue.py
+++ pc/main_blue.py
@@ -1,35 +1,50 @@
 import bluetooth
 import core
 
+first_message_size = 256 #in bytes, NOT USING BASE64
+fkey_message_size = 64 
+ping_message_size = 48 
+
 class My_Bluetooth_Server():
     def create_server(self):
         server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
         #port = bluetooth.get_available_port( bluetooth.RFCOMM )
         
+        #server_sock.bind(("",bluetooth.PORT_ANY))
+        port = 1
         server_sock.bind(("",bluetooth.PORT_ANY))
         server_sock.listen(1)
         #print "listening on port %d" % port
 
-        uuid = "1e0ca4ea-299d-4335-93eb-27fcfe7fa848"
-        bluetooth.advertise_service( server_sock, "SecureFiles", uuid )
+        #uuid = "1e0ca4ea-299d-4335-93eb-27fcfe7fa848"
+        #bluetooth.advertise_service( server_sock, "SecureFiles", uuid )
 
         self.server_sock = server_sock
 
-    def serve_connection(self, client_sock):
-        while True:
-            text = client_sock.recv(1024) # FIXME Times out? need to break when the smartphone stops sending data
-            #FIXME get sizes to receive
-            print "received [%s]" % text
-            try:
-                message = core.process_raw(text)
-            except Exception as e:
-                print e
-                break
+    def receive_message(self, size, client_sock):
+        text = client_sock.recv(size) # FIXME Times out? need to break when the smartphone stops sending data
 
-            client_sock.send("TEXT")
+        print "received [%s]" % text
+        try:
+            message = core.process_raw(text)
+        except Exception as e:
+            print e
+            return False
+        client_sock.send(message)
+        return True
+
+    def serve_connection(self, client_sock):
+        if not self.receive_message(first_message_size, client_sock):
+            return
+        if not self.receive_message(fkey_message_size, client_sock):
+            return
+        while self.receive_message(ping_message_size, client_sock):
+            pass
+            
 
     def run_forever(self):
+        print "Server started. Listening"
         server_sock = self.server_sock
         while True:
             client_sock,address = server_sock.accept()
@@ -39,11 +54,10 @@ class My_Bluetooth_Server():
 
             client_sock.close()
             print "Connection closed!!"
-
         server_sock.close()
 
 
+print "starting bluetooth server"
 server = My_Bluetooth_Server()
 server.create_server()
 server.run_forever()
-
