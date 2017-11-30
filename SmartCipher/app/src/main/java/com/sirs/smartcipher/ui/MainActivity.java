027ea880f26e10cbbe01328d89888b2648953914
TelmaCorreia <t.correia222@gmail.com>
2017-11-30 23:30:02 +0000

    qrcode working

--- SmartCipher/app/src/main/java/com/sirs/smartcipher/ui/MainActivity.java
+++ SmartCipher/app/src/main/java/com/sirs/smartcipher/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sirs.smartcipher.ui;
 
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
@@ -8,10 +9,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.sirs.smartcipher.Constants;
+import com.sirs.smartcipher.QRActivity;
 import com.sirs.smartcipher.R;
 import com.sirs.smartcipher.network.MyHttpClient;
+
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
@@ -24,7 +29,8 @@ import java.security.cert.CertificateException;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "running";
-    Button btn;
+    Button btn_start, btn_key;
+    TextView tv_log;
     int count =0;
     Boolean active = false;
     @Override
@@ -32,16 +38,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btn_start);
-        btn.setOnClickListener(new View.OnClickListener() {
+        btn_start = (Button) findViewById(R.id.btn_start);
+        btn_key = (Button) findViewById(R.id.btn_key);
+        tv_log = (TextView)findViewById(R.id.tv_log);
+
+        btn_start.setOnClickListener(new View.OnClickListener() {
             @TargetApi(Build.VERSION_CODES.O)
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View view) {
                 String url = "http://10.0.2.2";
-
                 Log.d(TAG, "*** client started ***");
-
+                String text = (String) tv_log.getText();
+                tv_log.setText(text + "Connection Started");
                 try {
 
                     if(!active) {
@@ -51,32 +60,27 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                     else {
-                        ; // Do nothing
+                        // Do nothing
                     }
                     return;
-/*
-                    MyHttpClient http = new MyHttpClient();
 
-                    if(count==0){ count ++; http.execute(url, Constants.START_CONNECTION);}
-                    else if(count==1){  http.execute(url, Constants.FKEY);}
-                    else{http.execute(url, Constants.PING);}
-//
-//                    http.execute(url, Constants.START_CONNECTION);
-//                    http.execute(url, Constants.FKEY);
-//                    while (true) {
-//                       http.execute(url, Constants.PING);
-//                        Thread.sleep(Constants.TIME_INTERVAL);
-//                    }
-*/
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-
                 Log.d(TAG, "*** client terminated ***");
             }
         });
 
+        btn_key.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, QRActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
 
 
