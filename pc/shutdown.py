b97672391724997970e476ca62a0bf5eb037c54a
filipepgm <filipepgm@hotmail.com>
2017-12-05 18:00:58 +0000

    pc: shutdown linux only

--- pc/shutdown.py
+++ pc/shutdown.py
@@ -2,27 +2,19 @@ import platform
 import atexit
 
 def register_function(f):
-    if "Windows" == platform.system():
-        try:
-            import win32api
-            win32api.SetConsoleCtrlHandler(f, True)
-        except Exception as e:
-            print e
-            print "FAILED TO REGISTER CONSOLE HANDLING"
-    else:
-        try:
-            import signal
+    try:
+        import signal
 
-            def stop(sig, frame):
-                print 'caught SIGTERM\n'
+        def stop(sig, frame):
+            print 'caught SIGTERM\n'
 
-            def ignore(sig, frsma):
-                print 'ignoring signal %d\n' % sig
-                
-            signal.signal(signal.SIGTERM, stop)
-            signal.signal(signal.SIGHUP, ignore)
-        except Exception as e:
-            print e
-            print "FAILED TO REGISTER SIGNAL HANDLING"
+        def ignore(sig, frsma):
+            print 'ignoring signal %d\n' % sig
+            
+        signal.signal(signal.SIGTERM, stop)
+        signal.signal(signal.SIGHUP, ignore)
+    except Exception as e:
+        print e
+        print "FAILED TO REGISTER SIGNAL HANDLING"
 
     atexit.register(f)
